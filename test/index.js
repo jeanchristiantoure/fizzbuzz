9cb2f8d9efef1d47ddaec2266bcc203baa1990bc
Keith Wade <keawade@gmail.com>
2016-08-30 19:37:07 -0500

    Add test support for Windows

--- test/index.js
+++ test/index.js
@@ -2,6 +2,7 @@ var readFile = require('fs').readFile;
 var assert = require('assert');
 var exec = require('child_process').exec;
 var join = require('path').join;
+var platform = require('os').platform;
 
 // Test the expected output.
 exec('node .', function(err, stdout, stderr) {
@@ -14,7 +15,7 @@ exec('node .', function(err, stdout, stderr) {
             throw err;
         }
 
-        var expected = exp.split('\r\n');
+        var expected = platform() == 'win32' ? exp.split('\r\n') : exp.split('\n');
         var actual = stdout.split('\n');
 
         for (var i = 0, mx = expected.length; i < mx; i++) {
